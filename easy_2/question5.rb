71a76daed8ce679e22feef22d2a0c640ad3de771
refilwe magwaneng <fyfymags@gmail.com>
2024-07-27 16:12:41 +0900

    mock exam 2

---
+++ easy_2/question5.rb
@@ -0,0 +1,5 @@
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+flintstones << "Dino"
+flintstones.concat(['Dino', 'Hoppy'])
+
+p flintstones
---
+++ easy_2/question8.rb
@@ -0,0 +1,6 @@
+advice = "Few things in life are as important as house training your pet dinosaur."
+p advice.size
+
+p return_value = advice.slice!(0..38) # make the return value:
+# p advice.slice!(39..72)
+p advice
