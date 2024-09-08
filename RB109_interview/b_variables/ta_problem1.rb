8d06881a70412ae81bf129742249d0f8f99b27d6
refilwe magwaneng <fyfymags@gmail.com>
2024-09-08 10:32:29 +0900

    hard problem exercise

--- RB109_interview/b_variables/ta_problem1.rb
+++ RB109_interview/b_variables/ta_problem1.rb
@@ -2,7 +2,7 @@ num = 25
 5.times do |x|
   5.times do |num|
     if num == 25
-    puts "#{x} * #{num} == 25"
+      puts "#{x} * #{num} == 25"
     end
   end
 end
---
+++ RB109_interview/d_variables/residence.rb
@@ -0,0 +1,5 @@
+town_residence = 'Osaka'
+
+3.times do |place|
+	  puts town_residence
+	end
